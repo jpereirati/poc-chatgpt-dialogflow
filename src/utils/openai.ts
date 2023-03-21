0f84c663d78ff561ce049fa419846fbf88d7a522
Lucas da Costa <39443576+ayato-tk@users.noreply.github.com>
2023-03-21 19:11:53 +0000

    Alteração no modelo e formatação do OpenAi

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -4,16 +4,10 @@ import { openai } from "..";
 async function textGeneration(prompt: string) {
   try {
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `Human: ${prompt}\nAI: `,
-      temperature: 0.9,
-      max_tokens: 500,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0.6,
-      stop: ["Human:", "AI:"],
+      model: "text-davinci-002-render-sha",
+      prompt
     });
-
+    
     return {
       status: 1,
       response: `${response.data.choices[0].text}`,
